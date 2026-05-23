5df222ce50f12139c2b34b3f766d9ccf9243559e
Gaotax2006 <zhuweiba9988@qq.com>
2026-05-23 15:02:15 +0800

    fix: Use SafeERC20 safeTransfer for all token transfers in TaskRouter (#181)

--- contracts/TaskRouter.sol
+++ contracts/TaskRouter.sol
@@ -2,8 +2,12 @@
 pragma solidity ^0.8.20;
 
 import "./AgentRegistry.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 contract TaskRouter {
+    using SafeERC20 for IERC20;
+
     AgentRegistry public registry;
 
     enum TaskStatus { Open, Assigned, Completed, Disputed, Cancelled }
