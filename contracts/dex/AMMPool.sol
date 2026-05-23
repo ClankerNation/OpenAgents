29ff41dc85c7921c0e7c725df43efb33468bc0db
Gaotax2006 <zhuweiba9988@qq.com>
2026-05-23 14:38:58 +0800

    fix: Add Permit2 support (#142)

--- contracts/dex/AMMPool.sol
+++ contracts/dex/AMMPool.sol
@@ -1,6 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
+interface IPermit2 {
+    function permitTransferFrom(
+        bytes calldata permit,
+        bytes calldata transfer,
+        address sender,
+        address recipient
+    ) external;
+}
+
+
 interface IERC20 {
     function transferFrom(address from, address to, uint256 amount) external returns (bool);
     function transfer(address to, uint256 amount) external returns (bool);
--- contracts/staking/StakingRewards.sol
+++ contracts/staking/StakingRewards.sol
@@ -1,6 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
+interface IPermit2 {
+    function permitTransferFrom(
+        bytes calldata permit,
+        bytes calldata transfer,
+        address sender,
+        address recipient
+    ) external;
+}
+
+
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
